017269aa931a13c8fdea45a22f6aabfc9bfa24f5
RadjenovicD96 <duka023zr@gmail.com>
2023-06-03 17:52:05 +0200

    Routing configuration

--- ancient-posts-client/src/app/app.module.ts
+++ ancient-posts-client/src/app/app.module.ts
@@ -5,10 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GraphQLModule } from './graphql.module';
 import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
+import { PostsComponent } from './posts/posts.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PostsComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
